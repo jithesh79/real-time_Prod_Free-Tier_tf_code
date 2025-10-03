c076223d39655d7634f581d71a3719b031dab946
jithesh79 <jitheshks11@gmail.com>
2025-10-03 09:58:29 +0530

    Create compute.tf

---
+++ compute.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "app" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+  associate_public_ip_address = false
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              echo "Hello from Terraform EC2" > /usr/share/nginx/html/index.html
+              EOF
+
+  tags = { Name = "${var.project_name}-ec2" }
+}
