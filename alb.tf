e1f5c572e113ce52b422477f870851af5a2541ed
jithesh79 <jitheshks11@gmail.com>
2025-10-03 09:59:12 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,38 @@
+resource "aws_lb" "app" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public.id]
+
+  enable_deletion_protection = false
+  tags = { Name = "${var.project_name}-alb" }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "tg_attach" {
+  target_group_arn = aws_lb_target_group.tg.arn
+  target_id        = aws_instance.app.id
+  port             = 80
+}
